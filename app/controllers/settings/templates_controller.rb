84fc1d380c765df84cedfd789c9f8be6293d97eb
yourloff <alex.yourloff@gmail.com>
2025-02-08 00:20:08 +0700

    Очищены стили. Добавлена страница Настроек. Исправлены иконки.

--- app/controllers/settings/templates_controller.rb
+++ app/controllers/settings/templates_controller.rb
@@ -1,41 +1,39 @@
-module Settings
-  class TemplatesController < ApplicationController
-    def index
-      @templates = Template.all
+class Settings::TemplatesController < ApplicationController
+  def index
+    @templates = Template.all
+  end
+
+  def new
+    @template = Template.new
+  end
+
+  def edit
+    @template = Template.find(params[:id])
+  end
+
+  def update
+    @template = Template.find(params[:id])
+
+    if @template.update(template_params)
+      redirect_to settings_templates_path, notice: 'Шаблон успешно обновлен!'
+    else
+      render :edit
     end
+  end
 
-    def new
-      @template = Template.new
+  def create
+    @template = Template.new(template_params)
+
+    if @template.save
+      redirect_to settings_templates_path, notice: 'Шаблон успешно загружен!'
+    else
+      render :new
     end
+  end
 
-    def edit
-      @template = Template.find(params[:id])
-    end
+  private
 
-    def update
-      @template = Template.find(params[:id])
-
-      if @template.update(template_params)
-        redirect_to settings_templates_path, notice: 'Шаблон успешно обновлен!'
-      else
-        render :edit
-      end
-    end
-
-    def create
-      @template = Template.new(template_params)
-
-      if @template.save
-        redirect_to settings_templates_path, notice: 'Шаблон успешно загружен!'
-      else
-        render :new
-      end
-    end
-
-    private
-
-    def template_params
-      params.require(:template).permit(:file, :title)
-    end
+  def template_params
+    params.require(:template).permit(:file, :title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  resources :settings, only: %i[index]
+
   namespace :settings do
     resources :templates
   end
