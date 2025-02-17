055371ffd7fe6d6a04a14004902d0f5f3fba8514
yourloff <alex.yourloff@gmail.com>
2025-02-17 20:54:49 +0700

    Настроена загрузка шаблонов через miniO

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,6 +1,6 @@
 class Template < ApplicationRecord
-  has_one_attached :file
-  has_one_attached :metadata
+  has_one_attached :file, service: :minio_templates, dependent: :destroy
+  has_one_attached :metadata, service: :minio_templates, dependent: :destroy
 
   validates :title, presence: true
 end
